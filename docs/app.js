6bfbbc0ba3eb80672faabdd879960ea08ed827fd
dldsudqlsdlqslek <tina4686@gmail.com>
2025-02-10 14:25:10 +0900

    feat: json

--- docs/app.js
+++ docs/app.js
@@ -1,11 +1,23 @@
 async function main() {
-  async function handleR() {
-    document.querySelector("#box").textContent = await (
-      await fetch("http://127.0.0.1:3000/")
-    ).text();
+  async function handleR(event) {
+    event.preventDefault();
+    const url = "http://127.0.0.1:3000/";
+    const formData = new FormData(document.querySelector("#rForm"));
+    const text = formData.get("text");
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+    document.querySelector("#box").textContent = JSON.stringify(json);
   }
 
-  document.querySelector("#rBtn").addEventListener("click", handleR, false);
+  document.querySelector("#rForm").addEventListener("submit", handleR, false);
 }
 
 //화면이 로드되면 이 함수를 실행시키겠다
