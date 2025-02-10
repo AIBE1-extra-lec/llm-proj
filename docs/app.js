2a734a46c214db0351621fe3202173538e84a606
dldsudqlsdlqslek <tina4686@gmail.com>
2025-02-10 16:41:10 +0900

    chore: modify code

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,10 @@
 async function main() {
   async function handleR(event) {
     event.preventDefault();
+    const spinner = document.createElement("div");
+    spinner.classList.add("spinner-border");
+    document.querySelector("#box").appendChild(spinner);
+
     const url = "http://127.0.0.1:3000/";
     const formData = new FormData(document.querySelector("#rForm"));
     const text = formData.get("text");
@@ -14,10 +18,22 @@ async function main() {
       },
     });
     const json = await response.json();
-    document.querySelector("#box").textContent = JSON.stringify(json);
+    //document.querySelector("#box").textContent = JSON.stringify(json);
+    spinner.remove();
+    const { image, desc } = json;
+    const box = document.querySelector("#box");
+    box.innerHTML = "";
+    const imageTag = document.createElement("img");
+    imageTag.classList.add("img-fluid");
+    imageTag.src = image;
+
+    const descTag = document.createElement("p");
+    descTag.textContent = desc;
+    box.appendChild(imageTag);
+    box.appendChild(descTag);
   }
 
-  document.querySelector("#rForm").addEventListener("submit", handleR, false);
+  document.querySelector("#rForm").addEventListener("submit", handleR);
 }
 
 //화면이 로드되면 이 함수를 실행시키겠다
